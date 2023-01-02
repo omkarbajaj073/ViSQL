568c2dd268af913054b2e9423b7e1e593c31897b
DrDrunkenstein42 <omkys.potatofries@gmail.com>
2023-01-02 18:17:43 +0530

    updated constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,9 @@
 # Metadata for connecting to mysql server
 
 # Local MySQL connection
-host = 'localhost'
-user = 'root'
-password = 'sql123'
+# host = 'localhost'
+# user = 'root'
+# password = 'sql123'
 
 # # Remote MySQL server connection
 # host = 'remotemysql.com'
@@ -11,11 +11,7 @@ password = 'sql123'
 # password = 'dwTF2uPRwT'
 
 # # Random other connection
-# user = "ardit700_student"
-# password = "ardit700_student"
-# host = "108.167.140.122"
-# database = "ardit700_pm1database"
-
+# https://docs.rfam.org/en/latest/database.html
 kwargs = {
   'host': 'mysql-rfam-public.ebi.ac.uk',
   'user': 'rfamro',
